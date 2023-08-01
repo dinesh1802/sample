8a497a45a242e2296c999a3a5dce91bdd2370ca2
dinesh.kumar.manjula <dineshkumar.1898@gmailcom>
2023-08-01 19:35:50 +0530

    updated test1

--- src/main/java/com/sample/demo/DemoApplication.java
+++ src/main/java/com/sample/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
   //from new branch
 	public static void main(String[] args) {
+		//updated by test1 repo
 		SpringApplication.run(DemoApplication.class, args);
 	}
 //changes in dinesh branch
