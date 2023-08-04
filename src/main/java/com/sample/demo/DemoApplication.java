5a7ed1806ac9e9c9fabaa44376ce328d2f74a54e
dinesh.kumar.manjula <dineshkumar.1898@gmailcom>
2023-08-04 19:57:36 +0530

    new joiner comment

--- src/main/java/com/sample/demo/DemoApplication.java
+++ src/main/java/com/sample/demo/DemoApplication.java
@@ -2,7 +2,7 @@ package com.sample.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//my branch
 @SpringBootApplication
 public class DemoApplication {//deleted comments
 	//RC1
